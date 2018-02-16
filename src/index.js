4f9963d6197d87b9fa14e1f27e0918b01a70225b
sebastianuskurniawan <sebastianus.kurniawan@gmail.com>
2018-02-16 15:21:21 +0700

    fix babel and initialize express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("hallo oi oi");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.send("Helloo"));
+app.listen(3000, () => console.log("Listengin to port 3000"));
