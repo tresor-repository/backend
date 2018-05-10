abffb3dca78f35c16d210c7a8df80e88c5309b4e
sebastianuskh <sebastianus.kurniawan@gmail.com>
2018-05-10 10:36:08 +0700

    add mailgun class

--- src/mailer/mailgun.js
+++ src/mailer/mailgun.js
@@ -1,3 +1,4 @@
 import mailgun from 'mailgun.js';
+import config from '../config.json';
 
-const mg = mailgun.client({username: 'api', key: });
+const mg = mailgun.client({username: 'api', key: config.mailgunkey});
